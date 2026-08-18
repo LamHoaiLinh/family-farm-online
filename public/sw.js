593e9ec461502687c49a8aeda2c520b714b3c204
LamHoaiLinh <lamhoailinh105@gmail.com>
2026-08-18 16:46:49 +0700

    Tăng phiên bản Service Worker và xóa cache cũ

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,29 @@
-const CACHE = 'family-farm-shell-v1';
+const CACHE = 'family-farm-shell-v2';
 const SHELL = ['./', './index.html', './manifest.webmanifest'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))));
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   if (url.origin !== location.origin || event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).then(r => {
-    const clone = r.clone();
-    caches.open(CACHE).then(c => c.put(event.request, clone));
-    return r;
-  }).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
